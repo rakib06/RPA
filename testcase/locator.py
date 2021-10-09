803e075a931ec2f5e7b6dc9baa03257bb476847c
rakibul <rakibul.hsn47@gmail.com>
2021-10-09 13:43:18 +0600

    unitest case done

---
+++ testcase/locator.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators:
+    GO_BUTTON = (By.ID, "submit")
+
+
+class SearchResultPageLocators:
+    pass
--- testcase/main.py
+++ testcase/main.py
@@ -9,16 +9,18 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
         self.driver.get("http://www.python.org")
     
-    # def test_example(self):
-    #     print("Test")
-    #     assert True, "This is a assert test"
-
-    # def test_example_2(self):
-    #     assert True 
-
-    # def not_a_test(self):
-    #     print("this wont print")
+    # def test_title(self):
+    #     mainPage = page.MainPage()
+    #     assert mainPage.is_title_matches()
     
+    def test_search_python(self):
+        mainPage = page.MainPage(self.driver)
+        assert mainPage.is_title_matches()
+        mainPage.search_text_element = "pycon"
+        mainPage.click_go_button()
+        search_result_page = page.SearchResultPage(self.driver)
+        assert search_result_page.is_results_found()
+
     # Close the browser 
     def tearDown(self):
         self.driver.close()
